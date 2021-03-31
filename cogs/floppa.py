520bc1ac685f179fe7a1aac8ab44ac4fd51828cf
Supreme-Watermelon <robom460@gmail.com>
2021-03-31 21:08:12 +0530

    fixed a major set of problems

--- cogs/floppa.py
+++ cogs/floppa.py
@@ -4,6 +4,36 @@ import os
 import json
 import random
 
+async def open_account(user):
+
+		users = await get_bank_data()
+
+		if str(user.id) in users:
+			return False 
+		else:
+			users[str(user.id)] = {}
+			users[str(user.id)]["wallet"] = 0
+			users[str(user.id)]["Floppa Republic Bank"] = 1000
+
+		with open("cogs/bank.json", "w") as f:
+			json.dump(users,f)
+
+async def get_bank_data():
+		with open("cogs/bank.json", "r") as f:
+			users = json.load(f)
+
+			return users
+
+async def update_bank(user, change=0, mode = "wallet"):
+		users = await get_bank_data()
+		users[str(user.id)][mode] += change
+
+		with open("cogs/bank.json", "w") as f:
+			json.dump(users,f)
+
+			bal = [users[str(user.id)]["wallet"], users[str(user.id)]["Floppa Republic Bank"]]
+
+		return bal
 
 class Economy(commands.Cog):
 	def __init__(self, client):
@@ -166,36 +196,7 @@ class Economy(commands.Cog):
 			await ctx.send("your flopping days are over")
 
 
-	async def open_account(user):
 
-		users = await get_bank_data()
-
-		if str(user.id) in users:
-			return False 
-		else:
-			users[str(user.id)] = {}
-			users[str(user.id)]["wallet"] = 0
-			users[str(user.id)]["Floppa Republic Bank"] = 1000
-
-		with open("bank.json", "w") as f:
-			json.dump(users,f)
-
-	async def get_bank_data():
-		with open("bank.json", "r") as f:
-			users = json.load(f)
-
-			return users
-
-	async def update_bank(user, change=0, mode = "wallet"):
-		users = await get_bank_data()
-		users[str(user.id)][mode] += change
-
-		with open("bank.json", "w") as f:
-			json.dump(users,f)
-
-			bal = [users[str(user.id)]["wallet"], users[str(user.id)]["Floppa Republic Bank"]]
-
-		return bal
 
 def setup(client):
 	client.add_cog(Economy(client))
